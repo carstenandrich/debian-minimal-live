b04735341434b5d7b6402d3cc5b5a014c3518e17
Carsten Andrich <carsten@andrich.name>
2020-04-04 17:25:57 +0200

    update debimg-helper-policy-rc.d

--- features.d/debimg-helper-policy-rc.d/usr/sbin/policy-rc.d
+++ features.d/debimg-helper-policy-rc.d/usr/sbin/policy-rc.d
@@ -1,7 +1,10 @@
 #!/bin/sh
 
-echo "************************************" >&2
-echo "All rc.d operations denied by policy" >&2
-echo "************************************" >&2
+# policy-rc.d script that denies all init operations through invoke-rc.d
+# https://manpages.debian.org/buster/init-system-helpers/invoke-rc.d.8.en.html
+
+echo "********* INVOKE-RC.D ACTION DENIED BY CHROOT-DISABLE-SERVICES PACKAGE *********" >&2
+echo "$*" >&2
+echo "********* INVOKE-RC.D ACTION DENIED BY CHROOT-DISABLE-SERVICES PACKAGE *********" >&2
 
 exit 101
