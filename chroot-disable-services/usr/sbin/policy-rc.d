27b7a16751fba30fcfc1672752d9ae16e430ffa1
Carsten Andrich <carsten@andrich.name>
2020-04-15 15:45:14 +0200

    move chroot-disable-services package

--- chroot-disable-services/usr/sbin/policy-rc.d
+++ chroot-disable-services/usr/sbin/policy-rc.d
@@ -4,7 +4,7 @@
 # https://manpages.debian.org/buster/init-system-helpers/invoke-rc.d.8.en.html
 
 echo "********* INVOKE-RC.D ACTION DENIED BY CHROOT-DISABLE-SERVICES PACKAGE *********" >&2
-echo "$*" >&2
+echo $0 $* >&2
 echo "********* INVOKE-RC.D ACTION DENIED BY CHROOT-DISABLE-SERVICES PACKAGE *********" >&2
 
 exit 101
