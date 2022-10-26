a3f583a2e01aab3a7711c064e582ff8a85f865c3
Carsten Andrich <carsten@andrich.name>
2022-10-26 23:42:31 +0200

    improve rootfs build process

--- rootfs.sh
+++ rootfs.sh
@@ -1,9 +1,12 @@
 #!/bin/sh -eux
 
+# TODO: derive this from bootstrapped system
+DEBIAN_SUITE="sid"
+
 unmount()
 {
 	# unmount bind mounts created by build process
-	for mount in "rootfs/var/cache/apt/archives" "rootfs/dev" "rootfs/proc" "rootfs/sys" ; do
+	for mount in "rootfs/var/cache/apt/archives" "rootfs/dev/pts" "rootfs/dev" "rootfs/proc" "rootfs/run" "rootfs/sys" "rootfs/tmp" ; do
 		if mountpoint -q "$mount" ; then
 			umount "$mount"
 		fi
@@ -30,9 +33,21 @@ EOF
 
 # bind mount current system into rootfs (read-only where possible)
 mount -o bind,ro /dev rootfs/dev
+mount -o bind,ro /dev/pts rootfs/dev/pts
 mount -o bind,ro /proc rootfs/proc
 mount -o bind,ro /sys rootfs/sys
-mount --bind /var/cache/apt/archives rootfs/var/cache/apt/archives
+mount -o bind,rw /var/cache/apt/archives rootfs/var/cache/apt/archives
+# mount empty tmpfs on /run and /tmp
+mount -t tmpfs -o rw,nosuid,nodev,noexec,mode=755 tmpfs rootfs/run
+mount -t tmpfs -o rw,mode=1777 tmpfs rootfs/tmp
+
+# installing systemd-resolved inside chroot will replace /etc/resolv.conf with
+# symlink to /run/systemd/resolve/stub-resolv.conf. with systemd-resolved not
+# running inside the chroot, this breaks DNS resolution.
+# Workaround: provide suitable *resolv.conf files in /run/systemd/resolve/
+mkdir -p rootfs/run/systemd/resolve/
+cat /etc/resolv.conf > rootfs/run/systemd/resolve/resolv.conf
+cat /etc/resolv.conf > rootfs/run/systemd/resolve/stub-resolv.conf
 
 # install rootfs-overlay package (handles overlayfs setup via initramfs scripts)
 dpkg --root=rootfs --install rootfs-overlay.deb
@@ -40,7 +55,12 @@ dpkg --root=rootfs --install rootfs-overlay.deb
 # call chroot build script with clean environment (to prevent locale issues, etc.)
 cp rootfs_chroot.sh rootfs/
 chmod +x rootfs/rootfs_chroot.sh
-env --ignore-environment PATH="/usr/sbin:/usr/bin:/sbin:/bin" TERM="$TERM" USER="$USER" chroot rootfs /rootfs_chroot.sh
+env --ignore-environment \
+		DEBIAN_SUITE="$DEBIAN_SUITE" \
+		PATH="/usr/sbin:/usr/bin:/sbin:/bin" \
+		TERM="$TERM" \
+		USER="$USER" \
+	chroot rootfs /rootfs_chroot.sh
 rm -rf rootfs/rootfs_chroot.sh
 
 # remove cdebootstrap helper that disables service invocation
--- rootfs_chroot.sh
+++ rootfs_chroot.sh
@@ -9,18 +9,32 @@ apt-get update
 apt-get --assume-yes install locales
 update-locale LANG=C.UTF-8
 
+# systemd-boot packaged separately since Debian Bookworm/Sid (systemd >= 251.2-3)
+# https://salsa.debian.org/systemd-team/systemd/-/blob/debian/251.2-3/debian/changelog
+# TODO: remove this workaround after stable release of Bookworm
+if [ $DEBIAN_SUITE = "bookworm" -o $DEBIAN_SUITE = "sid" ] ; then
+	systemd_boot="systemd-boot"
+fi
+
+# systemd-resolved packaged separately since Debian Bookworm/Sid (systemd >= 252.3-2)
+# https://salsa.debian.org/systemd-team/systemd/-/blob/debian/251.3-2/debian/systemd.NEWS
+# TODO: remove this workaround after stable release of Bookworm
+if [ $DEBIAN_SUITE = "bookworm" -o $DEBIAN_SUITE = "sid" ] ; then
+	systemd_resolved="systemd-resolved"
+fi
+
 # install subset of important packages plus some personal favorites
-# FIXME: as of systemd package version 251.2-3, systemd-boot was split off into separate package, see:
-#        https://salsa.debian.org/systemd-team/systemd/-/blob/debian/251.2-3/debian/changelog
-#        the systemd-boot package does not exist on current Debian stable (Bullseye) or prior versions
-apt-get --assume-yes --no-install-recommends install \
+apt-get --assume-yes --no-install-recommends -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" install \
 	bsdmainutils cpio dbus dmidecode init iproute2 \
 	kmod mount nano netbase sensible-utils \
-	systemd systemd-boot systemd-sysv systemd-timesyncd \
+	systemd ${systemd_boot:-} ${systemd_resolved:-} systemd-sysv systemd-timesyncd \
 	tzdata udev vim-common vim-tiny
 
+# enable systemd services not enabled by default
+systemctl enable systemd-networkd.service
+
 # install remaining packages
-apt-get --assume-yes --no-install-recommends install \
+apt-get --assume-yes --no-install-recommends -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" install \
 	linux-image-amd64 firmware-linux firmware-realtek \
 	bash-completion busybox cdebootstrap console-setup keyboard-configuration pciutils usbutils \
 	file htop less lshw psmisc screen sudo man-db manpages zstd \
@@ -33,17 +47,6 @@ apt-get --assume-yes --no-install-recommends install \
 	linux-perf python3 strace usb-modeswitch \
 	\
 	kitty policykit-1 sway swayidle swaylock sway-backgrounds wofi
-
-
-# install systemd-resolved last (breaks DNS resolution)
-# FIXME: as of systemd package version 252.3-2 systemd-resolved was split off into separate package, see:
-#        https://salsa.debian.org/systemd-team/systemd/-/blob/debian/251.3-2/debian/systemd.NEWS
-#        the systemd-resolved package does not exist on current Debian stable (Bullseye) or prior versions
-# FIXME: installing systemd-resolved overwrites /etc/resolv.conf, breaking DNS
-#        resolution, because resolved won't be running inside the chroot
-#        (service invocation is inhibited) and may not be running outside of it.
-apt-get --assume-yes --no-install-recommends install \
-	systemd-resolved
 
 # configure keyboard layout
 cat > /etc/default/keyboard <<-EOF
@@ -67,10 +70,3 @@ dpkg-reconfigure initramfs-tools
 useradd --create-home -d /home/user -s /bin/bash -G audio,dialout,input,sudo,video user
 usermod --password '$1$$oCLuEVgI1iAqOA8pwkzAg1' root
 usermod --password '$1$$ex9cQFo.PV11eSLXJFZuj.' user
-
-# use systemd services
-systemctl enable systemd-networkd.service
-systemctl enable systemd-resolved.service
-systemctl enable systemd-timesyncd.service
-# TODO: link to stub-resolv.conf instead?
-ln -fs /run/systemd/resolve/resolv.conf /etc/resolv.conf
